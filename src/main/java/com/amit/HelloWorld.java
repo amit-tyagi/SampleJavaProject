d9a2b3c823d181e2b47be5036e56e51d8519e08d
Amit <iamittyagi@gmail.com>
2013-06-02 18:25:14 -0500

    Initial Commit

---
+++ src/main/java/com/amit/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.amit;
+
+/**
+ * User: Amit
+ * Date: 6/2/13
+ * Time: 6:22 PM
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
