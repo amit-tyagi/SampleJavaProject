71e9b34d361b2b2f81c771f49f2fdf2d299e3bcc
Amit Tyagi <iamittyagi@gmail.com>
2013-06-12 13:36:13 -0500

    Changed the Java Program

--- src/main/java/com/amit/HelloWorld.java
+++ src/main/java/com/amit/HelloWorld.java
@@ -8,5 +8,8 @@ package com.amit;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Amit!!");
+		for(int i = 0; i < 10; i++) {
+			System.out.println("Counter: " + i);
+		}
     }
 }
